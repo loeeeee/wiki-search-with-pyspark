420232ce98516aac69759c0b31b425a57e8842de
loeeeee <95266635+loeeeee@users.noreply.github.com>
2026-02-23 12:18:54 -0600

    add django auto complete

--- shell.nix
+++ shell.nix
@@ -1,5 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  django-bash-completion = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/django/django/refs/heads/main/extras/django_bash_completion";
+    # A dummy hash is provided here. Nix will fail on the first run and provide the correct hash.
+    sha256 = "sha256-fo4jzovjyffoKQDdz3k4qiAsT+QuhzdPCe0l+bogE88=";
+  };
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     # Python 3.14
@@ -28,6 +35,9 @@ pkgs.mkShell {
     export JAVA_HOME=${pkgs.openjdk21}
 
     echo "Python version: $(python --version)"
+
+    # Source the completion script directly from the read-only Nix store
+    source ${django-bash-completion}
 
     source .venv/bin/activate
     echo "entering UV environment"
